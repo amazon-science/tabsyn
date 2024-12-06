9e7d928f61edb204baeeedf23e08bb7747bfeca1
AjayTalati76 <ajaytalati1976@gmail.com>
2024-12-06 08:33:42 +0000

    Add files via upload
    Added tensorboard functionality, and got ChatGPT to refactor
    
    moved num_epochs and batch_size to top of script,..., need to figure out why I cannot parse them in?

--- tabsyn/vae/main.py
+++ tabsyn/vae/main.py
@@ -4,9 +4,10 @@ import torch.nn as nn
 
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.tensorboard import SummaryWriter  # Added for TensorBoard
+
 import argparse
 import warnings
-
 import os
 from tqdm import tqdm
 import json
@@ -17,7 +18,6 @@ from utils_train import preprocess, TabularDataset
 
 warnings.filterwarnings('ignore')
 
-
 LR = 1e-3
 WD = 0
 D_TOKEN = 4
@@ -26,7 +26,6 @@ TOKEN_BIAS = True
 N_HEAD = 1
 FACTOR = 32
 NUM_LAYERS = 2
-
 
 def compute_loss(X_num, X_cat, Recon_X_num, Recon_X_cat, mu_z, logvar_z):
     ce_loss_fn = nn.CrossEntropyLoss()
@@ -38,33 +37,30 @@ def compute_loss(X_num, X_cat, Recon_X_num, Recon_X_cat, mu_z, logvar_z):
     for idx, x_cat in enumerate(Recon_X_cat):
         if x_cat is not None:
             ce_loss += ce_loss_fn(x_cat, X_cat[:, idx])
-            x_hat = x_cat.argmax(dim = -1)
-        acc += (x_hat == X_cat[:,idx]).float().sum()
+            x_hat = x_cat.argmax(dim=-1)
+        acc += (x_hat == X_cat[:, idx]).float().sum()
         total_num += x_hat.shape[0]
-    
+
     ce_loss /= (idx + 1)
     acc /= total_num
-    # loss = mse_loss + ce_loss
 
     temp = 1 + logvar_z - mu_z.pow(2) - logvar_z.exp()
-
     loss_kld = -0.5 * torch.mean(temp.mean(-1).mean())
     return mse_loss, ce_loss, loss_kld, acc
-
 
 def main(args):
     dataname = args.dataname
     data_dir = f'data/{dataname}'
-
     max_beta = args.max_beta
     min_beta = args.min_beta
     lambd = args.lambd
-
-    device =  args.device
-
+    device = args.device
+    #num_epochs = args.num_epochs
+    #batch_size = args.batch_size
+    num_epochs = 6000 
+    batch_size = 4096
 
     info_path = f'data/{dataname}/info.json'
-
     with open(info_path, 'r') as f:
         info = json.load(f)
 
@@ -77,51 +73,30 @@ def main(args):
     encoder_save_path = f'{ckpt_dir}/encoder.pt'
     decoder_save_path = f'{ckpt_dir}/decoder.pt'
 
-    X_num, X_cat, categories, d_numerical = preprocess(data_dir, task_type = info['task_type'])
-
-    X_train_num, _ = X_num
-    X_train_cat, _ = X_cat
-
+    X_num, X_cat, categories, d_numerical = preprocess(data_dir, task_type=info['task_type'])
     X_train_num, X_test_num = X_num
     X_train_cat, X_test_cat = X_cat
 
-    X_train_num, X_test_num = torch.tensor(X_train_num).float(), torch.tensor(X_test_num).float()
-    X_train_cat, X_test_cat =  torch.tensor(X_train_cat), torch.tensor(X_test_cat)
+    X_train_num = torch.tensor(X_train_num).float()
+    X_test_num = torch.tensor(X_test_num).float().to(device)
+    X_train_cat = torch.tensor(X_train_cat)
+    X_test_cat = torch.tensor(X_test_cat).to(device)
 
+    train_data = TabularDataset(X_train_num, X_train_cat)
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=4)
 
-    train_data = TabularDataset(X_train_num.float(), X_train_cat)
-
-    X_test_num = X_test_num.float().to(device)
-    X_test_cat = X_test_cat.to(device)
-
-    batch_size = 4096
-    train_loader = DataLoader(
-        train_data,
-        batch_size = batch_size,
-        shuffle = True,
-        num_workers = 4,
-    )
-
-    model = Model_VAE(NUM_LAYERS, d_numerical, categories, D_TOKEN, n_head = N_HEAD, factor = FACTOR, bias = True)
-    model = model.to(device)
-
-    pre_encoder = Encoder_model(NUM_LAYERS, d_numerical, categories, D_TOKEN, n_head = N_HEAD, factor = FACTOR).to(device)
-    pre_decoder = Decoder_model(NUM_LAYERS, d_numerical, categories, D_TOKEN, n_head = N_HEAD, factor = FACTOR).to(device)
-
-    pre_encoder.eval()
-    pre_decoder.eval()
-
+    model = Model_VAE(NUM_LAYERS, d_numerical, categories, D_TOKEN, n_head=N_HEAD, factor=FACTOR, bias=True).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=LR, weight_decay=WD)
     scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.95, patience=10, verbose=True)
 
-    num_epochs = 4000
+    writer = SummaryWriter(log_dir=f'runs/{dataname}_{int(time.time())}')  # TensorBoard initialization
+
     best_train_loss = float('inf')
-
     current_lr = optimizer.param_groups[0]['lr']
-    patience = 0
-
     beta = max_beta
+    patience = 0
     start_time = time.time()
+
     for epoch in range(num_epochs):
         pbar = tqdm(train_loader, total=len(train_loader))
         pbar.set_description(f"Epoch {epoch+1}/{num_epochs}")
@@ -129,7 +104,6 @@ def main(args):
         curr_loss_multi = 0.0
         curr_loss_gauss = 0.0
         curr_loss_kl = 0.0
-
         curr_count = 0
 
         for batch_num, batch_cat in pbar:
@@ -140,7 +114,6 @@ def main(args):
             batch_cat = batch_cat.to(device)
 
             Recon_X_num, Recon_X_cat, mu_z, std_z = model(batch_num, batch_cat)
-        
             loss_mse, loss_ce, loss_kld, train_acc = compute_loss(batch_num, batch_cat, Recon_X_num, Recon_X_cat, mu_z, std_z)
 
             loss = loss_mse + loss_ce + beta * loss_kld
@@ -151,50 +124,52 @@ def main(args):
             curr_count += batch_length
             curr_loss_multi += loss_ce.item() * batch_length
             curr_loss_gauss += loss_mse.item() * batch_length
-            curr_loss_kl    += loss_kld.item() * batch_length
+            curr_loss_kl += loss_kld.item() * batch_length
 
         num_loss = curr_loss_gauss / curr_count
         cat_loss = curr_loss_multi / curr_count
         kl_loss = curr_loss_kl / curr_count
-        
 
-        '''
-            Evaluation
-        '''
+        # Evaluation
         model.eval()
         with torch.no_grad():
             Recon_X_num, Recon_X_cat, mu_z, std_z = model(X_test_num, X_test_cat)
-
             val_mse_loss, val_ce_loss, val_kl_loss, val_acc = compute_loss(X_test_num, X_test_cat, Recon_X_num, Recon_X_cat, mu_z, std_z)
-            val_loss = val_mse_loss.item() * 0 + val_ce_loss.item()    
 
-            scheduler.step(val_loss)
-            new_lr = optimizer.param_groups[0]['lr']
+        # TensorBoard logging
+        writer.add_scalar('Loss/Train MSE', num_loss, epoch)
+        writer.add_scalar('Loss/Train CE', cat_loss, epoch)
+        writer.add_scalar('Loss/Train KL', kl_loss, epoch)
+        writer.add_scalar('Loss/Val MSE', val_mse_loss.item(), epoch)
+        writer.add_scalar('Loss/Val CE', val_ce_loss.item(), epoch)
+        writer.add_scalar('Accuracy/Train ACC', train_acc.item(), epoch)
+        writer.add_scalar('Accuracy/Val ACC', val_acc.item(), epoch)
 
-            if new_lr != current_lr:
-                current_lr = new_lr
-                print(f"Learning rate updated: {current_lr}")
-                
-            train_loss = val_loss
-            if train_loss < best_train_loss:
-                best_train_loss = train_loss
-                patience = 0
-                torch.save(model.state_dict(), model_save_path)
-            else:
-                patience += 1
-                if patience == 10:
-                    if beta > min_beta:
-                        beta = beta * lambd
+        # Update learning rate and beta
+        scheduler.step(val_ce_loss.item())
+        new_lr = optimizer.param_groups[0]['lr']
+        if new_lr != current_lr:
+            current_lr = new_lr
+            print(f"Learning rate updated: {current_lr}")
+        if val_ce_loss.item() < best_train_loss:
+            best_train_loss = val_ce_loss.item()
+            patience = 0
+            torch.save(model.state_dict(), model_save_path)
+        else:
+            patience += 1
+            if patience == 10 and beta > min_beta:
+                beta *= lambd
 
-
-        # print('epoch: {}, beta = {:.6f}, Train MSE: {:.6f}, Train CE:{:.6f}, Train KL:{:.6f}, Train ACC:{:6f}'.format(epoch, beta, num_loss, cat_loss, kl_loss, train_acc.item()))
-        print('epoch: {}, beta = {:.6f}, Train MSE: {:.6f}, Train CE:{:.6f}, Train KL:{:.6f}, Val MSE:{:.6f}, Val CE:{:.6f}, Train ACC:{:6f}, Val ACC:{:6f}'.format(epoch, beta, num_loss, cat_loss, kl_loss, val_mse_loss.item(), val_ce_loss.item(), train_acc.item(), val_acc.item() ))
+        print(f'epoch: {epoch}, beta: {beta:.6f}, Train MSE: {num_loss:.6f}, Train CE: {cat_loss:.6f}, Train KL: {kl_loss:.6f}, Val MSE: {val_mse_loss.item():.6f}, Val CE: {val_ce_loss.item():.6f}, Train ACC: {train_acc.item():.6f}, Val ACC: {val_acc.item():.6f}')
 
     end_time = time.time()
-    print('Training time: {:.4f} mins'.format((end_time - start_time)/60))
-    
+    print(f'Training time: {(end_time - start_time) / 60:.4f} mins')
+
     # Saving latent embeddings
     with torch.no_grad():
+        pre_encoder = Encoder_model(NUM_LAYERS, d_numerical, categories, D_TOKEN, n_head=N_HEAD, factor=FACTOR).to(device)
+        pre_decoder = Decoder_model(NUM_LAYERS, d_numerical, categories, D_TOKEN, n_head=N_HEAD, factor=FACTOR).to(device)
+
         pre_encoder.load_weights(model)
         pre_decoder.load_weights(model)
 
@@ -207,20 +182,21 @@ def main(args):
         print('Successfully load and save the model!')
 
         train_z = pre_encoder(X_train_num, X_train_cat).detach().cpu().numpy()
-
         np.save(f'{ckpt_dir}/train_z.npy', train_z)
 
         print('Successfully save pretrained embeddings in disk!')
 
+    writer.close()  # Close TensorBoard writer
+
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(description='Variational Autoencoder')
-
     parser.add_argument('--dataname', type=str, default='adult', help='Name of dataset.')
     parser.add_argument('--gpu', type=int, default=0, help='GPU index.')
     parser.add_argument('--max_beta', type=float, default=1e-2, help='Initial Beta.')
     parser.add_argument('--min_beta', type=float, default=1e-5, help='Minimum Beta.')
     parser.add_argument('--lambd', type=float, default=0.7, help='Decay of Beta.')
+    parser.add_argument('--num_epochs', type=int, default=6000, help='Number of training epochs.')
+    parser.add_argument('--batch_size', type=int, default=4096, help='Batch size for training.')
 
     args = parser.parse_args()
 
